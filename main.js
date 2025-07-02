7f066b4822c5a2c69406378be292d8e44a375109
HanaWebdesign <hanawebdesign26@gmail.com>
2025-07-03 07:19:30 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
-// ページ読み込み後にスクロールイベントを追加
-window.addEventListener("scroll", () => {
-  const logo = document.querySelector(".hero-logo");
-  const triggerPoint = window.innerHeight / 1.2;
-
-  if (logo) {
-    const logoTop = logo.getBoundingClientRect().top;
-    if (logoTop < triggerPoint) {
-      logo.classList.add("show");
-    }
-  }
-});
-
-
-window.addEventListener("load", () => {
-  document.querySelector('.hero-logo').classList.add('show');
-});
-
 document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.getElementById("hamburger");
   const navMenu = document.getElementById("nav-menu");
 
   hamburger.addEventListener("click", function () {
     navMenu.classList.toggle("active");
+  });
+
+  // ページ読み込み時にロゴを表示する
+  const logo = document.querySelector(".hero-logo");
+  if (logo) {
+    logo.classList.add("show");
+  }
+
+  // スクロール時にロゴを表示（アニメーション用）
+  window.addEventListener("scroll", () => {
+    const triggerPoint = window.innerHeight / 1.2;
+    const logoTop = logo.getBoundingClientRect().top;
+    if (logoTop < triggerPoint) {
+      logo.classList.add("show");
+    }
   });
 });
